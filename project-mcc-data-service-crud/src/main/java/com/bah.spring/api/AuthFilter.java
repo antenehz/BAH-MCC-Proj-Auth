b09515e670858fda0f6d39ccf7041389530f15ba
Anteneh Zeleke <antenehzeleke3@gmail.com>
2022-10-19 12:54:33 -0600

    added auth files

--- project-mcc-data-service-crud/src/main/java/com/bah.spring/api/AuthFilter.java
+++ project-mcc-data-service-crud/src/main/java/com/bah.spring/api/AuthFilter.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 public class AuthFilter implements Filter {
 
     //JWTUtil jwtUtil = new JWTMockUtil();
-    JWTUtil jwtUtil;
+     JWTUtil jwtUtil = new JWTHelper();;
 
     private String api_scope = "com.bah.spring.api.customer.r";
 
@@ -27,7 +27,7 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
-        if (uri.startsWith("/token")) {
+        if (uri.startsWith("/api/token")) {
             // continue on to get-token endpoint
             chain.doFilter(request, response);
             return;
